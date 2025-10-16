31380bad5061da65b72dcc817999405d5d2f0d82
Muhammad Saqib Doullah <saqibdoullah@gmail.com>
2025-10-16 17:57:17 +0000

    it is still not fixed. It is normal when scrolling down

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import TopNav from './top-nav';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
         <div className="mr-8 hidden md:flex items-center gap-2">
            <Link href="/" className="flex items-center gap-2">
