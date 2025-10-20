16490ecf1b5a3941938caca5fa5495b060f95246
Muhammad Saqib Doullah <saqibdoullah@gmail.com>
2025-10-20 19:01:22 +0000

    I have clicked on invoices and it showed me this

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import InvoiceList from "./invoice-list";
+import InvoiceList from "./list/invoice-list";
 
 export const metadata = {
   title: "Invoices",
