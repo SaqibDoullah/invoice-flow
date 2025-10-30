f5222d263621e513e7b965e3c8d5e0b131f36185
Muhammad Saqib Doullah <saqibdoullah@gmail.com>
2025-10-30 07:00:27 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Element type is

--- src/app/inventory/transfers/transfers-page-content.tsx
+++ src/app/inventory/transfers/transfers-page-content.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Home, ChevronRight, Search, MessageCircle, ChevronDown, Truck, Undo2, RefreshCw, Filter, ArrowUpDown, ShieldAlert, Upload, Download, Settings2, Checkbox, ArrowDown } from 'lucide-react';
+import { Home, ChevronRight, Search, MessageCircle, ChevronDown, Truck, Undo2, RefreshCw, Filter, ArrowUpDown, ShieldAlert, Upload, Download, Settings2, Checkbox, ArrowDown, ArrowUp } from 'lucide-react';
 import { collection, query, onSnapshot, orderBy, limit, where } from 'firebase/firestore';
 import { format, isValid } from 'date-fns';
 import Image from 'next/image';
@@ -381,8 +381,3 @@ export default function TransfersPageContent() {
         </AuthGuard>
     );
 }
-
-const ArrowUp = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
-)
-
