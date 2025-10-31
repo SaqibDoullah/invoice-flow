97749dae731ad9aeed36391fc291d5b5eb79b07f
Muhammad Saqib Doullah <saqibdoullah@gmail.com>
2025-10-31 15:40:09 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.stickpng.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
