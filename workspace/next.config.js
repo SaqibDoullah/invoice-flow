6050f7e35044b8b0cd5b62ff40fc8d8371c7f56b
Muhammad Saqib Doullah <saqibdoullah@gmail.com>
2025-11-15 00:08:59 +0000

    see this error

---
+++ workspace/next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.stickpng.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
